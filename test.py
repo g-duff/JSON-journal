3e40b22cb28264398124d9982d9e15ba99f0ef86
Lebbitt <lucy.ebbitt@gmail.com>
2023-01-07 13:55:03 +0000

    Add tests for functions

---
+++ test.py
@@ -0,0 +1,43 @@
+import unittest
+from read_data import separate_expenses
+from read_data import total_balance
+
+class TestSeparateExpenses(unittest.TestCase):
+
+    def test_simplecase(self):
+        # Given
+        journal = [
+            {
+                'expenses:eatingout': 100, 'assets:current' : -100, 
+                'expenses:groceries' : 50, 'assets:current' : -50
+            },
+        ]
+        expected_expenses = {
+            'expenses:eatingout': 100,
+            'expenses:groceries': 50,
+        }
+
+        # When
+        actual_expenses = separate_expenses(journal)
+
+        # Then
+        self.assertEqual(expected_expenses, actual_expenses)
+
+
+class TestTotalBalance(unittest.TestCase):
+
+    def test_simplecase(self):
+        # Given
+        journal = [
+            {
+                'income:job': -1000, 'assets:current' : 1000, 
+                'expenses:groceries' : 50, 'assets:current' : -50
+            }
+        ]
+        expected_total = 950  #(income * (-1)) - expenses) 
+
+        # When
+        actual_total = total_balance(journal)
+
+        # Then
+        self.assertEqual(expected_total, actual_total)

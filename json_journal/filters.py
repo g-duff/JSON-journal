07e3c1bfdc9d6cc8757727bde4b1f8e596b3240d
Lebbitt <lucy.ebbitt@gmail.com>
2023-01-07 20:06:38 +0000

    Change dictionary comprehension

--- json_journal/filters.py
+++ json_journal/filters.py
@@ -1,4 +1,3 @@
 def filter_balances_to_expenses(all_balances):
-    expenses = [[account, all_balances[account]] for account in all_balances if account.startswith("expense:")]
-    expenses_dict = dict(expenses)
-    return expenses_dict
+    expenses = {account: all_balances[account] for account in all_balances if account.startswith("expense:")}
+    return expenses

f9877e389f11416de4ff5a275be6c39389feda95
Lebbitt <lucy.ebbitt@gmail.com>
2023-01-08 14:37:51 +0000

    Add documentation to charts file

--- json_journal/charts.py
+++ json_journal/charts.py
@@ -1,6 +1,9 @@
-# pylint: disable = import-error, missing-class-docstring, missing-function-docstring, missing-module-docstring
+'''Create pie chart'''
 
 def create_pie_chart(balances, axes):
+    '''balances -> dictionary of full account names and balances for each account,
+    axes -> specifed axes for plotting the pie chart.
+    Returns pie chart ready to be shown (n.b. plt.show needs to be called on this pie chart)'''
     account_names = []
     account_balances = []
     for account_name, account_balance in balances.items():

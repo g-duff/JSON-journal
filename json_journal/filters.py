b96ac771cd5820f8f0c139ee8458e2bcfc4effee
Lebbitt <lucy.ebbitt@gmail.com>
2023-01-08 14:18:53 +0000

    Add documentation to filters file

--- json_journal/filters.py
+++ json_journal/filters.py
@@ -1,6 +1,8 @@
-# pylint: disable = import-error, missing-class-docstring, missing-function-docstring, missing-module-docstring
+'''Filter balances to expenses'''
 
 def filter_balances_to_expenses(all_balances):
+    '''all_balances -> dictionary with full account name and corresponding balance.
+    Returns dictionary of balances where the account name starts with expense'''
     expenses = {account: all_balances[account]
                 for account in all_balances if account.startswith("expense:")}
     return expenses

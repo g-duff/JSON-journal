f13812a624d36682e2cc765832845e323e11450a
Lebbitt <lucy.ebbitt@gmail.com>
2023-01-08 13:54:24 +0000

    Add encoding to open file

--- json_journal/file_io.py
+++ json_journal/file_io.py
@@ -3,12 +3,13 @@ import json
 
 
 def load_json_file(file_path):
-    with open(file_path, 'r') as file:
+    '''documentation'''
+    with open(file_path, 'r', encoding="utf-8") as file:
         data = json.load(file)
     return data
 
 
 def save_json_file(all_data, file_path):
     json_string = json.dumps(all_data, indent=4)
-    with open(file_path, 'w+') as file:
+    with open(file_path, 'w+', encoding="utf-8") as file:
         file.write(json_string)

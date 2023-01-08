c567e7e3ca282e4a35a997d36d065448121684f0
Lebbitt <lucy.ebbitt@gmail.com>
2023-01-08 14:14:05 +0000

    Add documentation to tabulate data file

--- json_journal/tabulate_data.py
+++ json_journal/tabulate_data.py
@@ -1,8 +1,11 @@
-# pylint: disable = import-error, missing-class-docstring, missing-function-docstring, missing-module-docstring
+'''Tabulate data'''
 from tabulate import tabulate
 
 
 def tabulate_sorted_entries(all_entries, sorted_accounts):
+    '''all_entries -> dictionary with the full account name and corresponding balance,
+    sorted_accounts -> parent account names sorted alphabetically.
+    Returns a tabulated table printed in the console'''
     output_rows = [[account_name, all_entries[account_name]]
                    for account_name in sorted_accounts]
     output_headers = ["Account", "Amount"]

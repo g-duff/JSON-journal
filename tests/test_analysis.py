c4d95dbb63a50d2c747d97096fb7a436c3969384
Lebbitt <lucy.ebbitt@gmail.com>
2023-01-19 21:36:12 +0000

    Add test for cumulative profit function

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -1,6 +1,6 @@
 # pylint: disable = import-error, missing-class-docstring, missing-function-docstring, missing-module-docstring
 import unittest
-from json_journal.data_analysis import total_profit
+from json_journal import data_analysis
 
 
 class TestTotalProfit(unittest.TestCase):
@@ -17,7 +17,51 @@ class TestTotalProfit(unittest.TestCase):
         expected_profit = (expected_income * (-1)) - expected_expenses
 
         # When
-        actual_profit = total_profit(balances)
+        actual_profit = data_analysis.total_profit(balances)
 
         # Then
         self.assertEqual(expected_profit, actual_profit)
+
+
+class TestCumulativeProfit(unittest.TestCase):
+
+    def test_simplecase(self):
+        # Given
+        ledger = [
+            {
+                "date" : "2022-11-01",
+                "description" : "pay day",
+                "entries" : [
+                    {"account" : "income:job", "amount" : -2000},
+                    {"account" : "assets:tsb", "amount" : 2000}
+                ]
+            } ,
+    
+            {
+                "date" : "2022-11-01",
+                "description" : "food",
+                "entries" : [
+                {"account" : "expense:food", "amount" : 20},
+                {"account" : "assets:tsb", "amount" : -20}
+            ]
+        } , 
+    
+        {
+            "date" : "2022-11-03",
+            "description" : "gym membership",
+            "entries" : [
+                {"account" : "expense:gym", "amount" : 20},
+                {"account" : "assets:tsb", "amount" : -20}
+            ]
+        } ]
+
+        expected_dates = ['2022-11-01', '2022-11-03']
+        expected_cumulative_profits = [1980, 1960]
+
+        # When 
+        actual_dates = data_analysis.cumulative_profit(ledger)[0]
+        actual_cumulative_profit = data_analysis.cumulative_profit(ledger)[1]
+
+        # Then
+        self.assertEqual(expected_dates, actual_dates)
+        self.assertEqual(expected_cumulative_profits, actual_cumulative_profit)

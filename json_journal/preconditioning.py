72304b1d0e80f9e161beedf1fe883cbd5bbd8e8f
Lebbitt <lucy.ebbitt@gmail.com>
2023-01-29 19:28:32 +0000

    Add check if entries sum to zero

--- json_journal/preconditioning.py
+++ json_journal/preconditioning.py
@@ -18,5 +18,31 @@ def sort_journal_by_date(ledger):
     return sorted_ledger
 
 
+def check_entries_sum(ledger):
+    '''
+    Check entries in a transaction sum to zero.
 
+    Parameters
+    ----------
+    Ledger : dict
+        Loaded json file.
+
+    Returns
+    -------
+    Amount_sum : int
+        The sum of entry amounts in each transaction.
+    '''
+    amount_sum = 0
+    for transaction in ledger:
+        entries = transaction['entries']
+
+
+        for entry in entries:
+            amount = entry['amount']
+            amount_sum += amount
+    
+    if amount_sum == 0:
+        print("Entries sum to zero")
+    else:
+        print("Entries do not sum to zero, but ", amount_sum)
 

44934f64b5c2e0db5fef5151c2d7f8dbda315933
george <george.duffett@icloud.com>
2023-03-07 21:51:10 +0000

    Add agent, lint, test stages

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,10 +1,25 @@
 pipeline {
-    agent any 
-    stages {
-        stage('Test') { 
-            steps {
-                sh "echo hello world"
-            }
-        }
-    }
+	agent {
+		docker { image 'python:slim-bullseye' }
+	}
+	environment {
+		HOME="${env.WORKSPACE}"
+	}
+	stages {
+		stage('Install dependencies') {
+			steps {
+				sh "python3 -m pip install --user --no-cache-dir --requirement ./requirements/dev.txt"
+			}
+		}
+		stage('Lint') {
+			steps {
+				sh "python3 -m pylint ./json_journal/*py ./tests/*py"
+			}
+		}
+		stage('Test') { 
+			steps {
+				sh "python3 -m unittest discover ./tests/ 'test_*.py'"
+			}
+		}
+	}
 }

7daf9fa9386cb3b82a15846551a7aeaffb8065a3
george <george.duffett@icloud.com>
2023-03-10 23:44:28 +0000

    Change header order in state update

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -27,9 +27,9 @@ pipeline {
 				sh '''curl -L \
 					-X POST \
 					-H "Accept: application/vnd.github+json" \
-					-H "Authorization: Bearer $TOKEN"\
 					-H "X-GitHub-Api-Version: 2022-11-28" \
 					"https://api.github.com/repos/g-duff/JSON-journal/statuses/$GIT_COMMIT" \
+					-H "Authorization: Bearer $TOKEN"\
 					-d '{"state":"success","context":"continuous-integration/jenkins"}'
 					'''
 			}
@@ -40,9 +40,9 @@ pipeline {
 				sh '''curl -L \
 					-X POST \
 					-H "Accept: application/vnd.github+json" \
-					-H "Authorization: Bearer $TOKEN"\
 					-H "X-GitHub-Api-Version: 2022-11-28" \
 					"https://api.github.com/repos/g-duff/JSON-journal/statuses/$GIT_COMMIT" \
+					-H "Authorization: Bearer $TOKEN"\
 					-d '{"state":"failure","context":"continuous-integration/jenkins"}'
 					'''
 			}

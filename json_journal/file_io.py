ff5e7b896a4ff3a00f94e39c49e22fa819dae29b
Lebbitt <lucy.ebbitt@gmail.com>
2023-03-26 20:32:23 +0100

    Add CSV to JSON function

--- json_journal/file_io.py
+++ json_journal/file_io.py
@@ -1,5 +1,6 @@
 '''Functions to interact with json files'''
 import json
+import csv
 
 
 def load_json_file(file_path):
@@ -39,3 +40,37 @@ def save_json_file(all_data, file_path):
     json_string = json.dumps(all_data, indent=4)
     with open(file_path, 'w+', encoding="utf-8") as file:
         file.write(json_string)
+
+
+# Add function for loading csv and turing it to json - example with monzo, how to do others - test - check for duplicates
+def csv_to_json(file):
+    '''
+    Convert a CSV file to a JSON.
+
+    Parameters
+    ----------
+    file : file path
+        File path for the CSV.
+
+    Returns
+    -------
+    json_file : dict
+        Returns a JSON format for the data.
+    '''
+    json_file = []
+
+    with open(file, 'r') as csvfile:
+        csv_file = csv.reader(csvfile)
+        next(csv_file, None)
+        for record in csv_file:
+            dict = {}
+            date = record[1]
+            description = record[4]
+            amount = float(record[7])
+            dict['date'] = date
+            dict['description'] = description
+            dict['amount'] = amount
+            json_file.append(dict)
+    
+    return json_file
+
